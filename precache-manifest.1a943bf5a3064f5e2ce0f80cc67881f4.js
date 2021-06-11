5de0b7b7e48b79653ca6c7c870effd8774504a65
Ghazian Fadhli Fakhrusy <fadhlighazian@gmail.com>
2021-06-11 14:31:33 +0700

    Menyalin isi folder build

---
+++ precache-manifest.1a943bf5a3064f5e2ce0f80cc67881f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f016eac0035c22c6e40a",
+    "url": "/static/css/main.04855ab6.chunk.css"
+  },
+  {
+    "revision": "f016eac0035c22c6e40a",
+    "url": "/static/js/main.f016eac0.chunk.js"
+  },
+  {
+    "revision": "1b58d379c1154b4531a5",
+    "url": "/static/js/1.1b58d379.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e99073b46e1e1e2bc783dbb42c71c1e6",
+    "url": "/index.html"
+  }
+];
